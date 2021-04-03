1fba25c5179fa96ff51dc1abb3c6594854443f03
M Kazi <35180184+VolcanoKing370@users.noreply.github.com>
2021-04-03 18:34:11 -0400

    Add files via upload
    Added the JS script

---
+++ script.js
@@ -0,0 +1,32 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoibWthemk3IiwiYSI6ImNrbTQ2ZzFlaTAxajAydXMzaWdla2dsMXoifQ.BSfCZ8BhdR8LOhDuqohbeg';
+
+navigator.geolocation.getCurrentPosition(successLoc, 
+    errorLoc, {
+        enableHighAccuracy: true
+})
+
+function successLoc(position){
+    mapSetup([position.coords.longitude, position.coords.latitude] )
+}
+
+function errorLoc(){
+    mapSetup([-2.24, 53.48])
+}
+
+function mapSetup(center){
+    const map = new mapboxgl.Map({
+        container: "map",
+        style: "mapbox://styles/mapbox/streets-v11",
+        center: center,
+        zoom : 15
+    })
+
+    const nav = new mapboxgl.NavigationControl();
+    map.addControl(nav, 'top-right');
+
+    var directions = new MapboxDirections({
+        accessToken: mapboxgl.accessToken
+    })
+
+    map.addControl(directions, 'top-left')
+}
